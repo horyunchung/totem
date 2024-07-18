3cc349d0586693f259dfd366233372b303beeca5
ho.chung@staff.uni-marburg.de <ho.chung@staff.uni-marburg.de>
2024-07-18 09:17:06 +0200

    prepare analysis for the paper

--- data/FigureX.R
+++ data/FigureX.R
@@ -1,9 +1,11 @@
 library(readxl)
+library(caper)
+tree <- read.nexus("data/mammalia.nexus")
 data <- read_xlsx("data/brv12350-sup-0003-tables2.xlsx", skip = 2)
 
-data <- subset(data, subset = SELECT == 1, select = c("BMR", "body mass"))
+data <- subset(data, subset = SELECT == 1, select = c("Species Nexus", "BMR", "body mass"))
 
-colnames(data) <- c("BMR", "Mass")
+colnames(data) <- c("Species nexus", "BMR", "Mass")
 
 data$BMR <- as.numeric(data$BMR)
 data$Mass <- as.numeric(data$Mass)
@@ -11,8 +13,7 @@ data$Mass <- as.numeric(data$Mass)
 
 tab <- toTab(data)
 
-## test allowmetric laws with fixed exponents
-
+## test pure allowmetric laws with fixed exponents
 testAllometric <- function(b){
   ## <x^b>
   meanX <- sum(tab$f * tab$Mass^b)
@@ -22,26 +23,43 @@ testAllometric <- function(b){
     ## hypothesis constraint
     tab$BMR - tab$BMR / tab$Mass^b * meanX,
     ## structural constraint
+    ### <x^b>_f = <x^b>_p
     tab$Mass^b,
+    ### normalization
     rep(1, nrow(tab))
   )
-
+  ## expected values if the theory is
+  ## correct
   targets <- c(0, meanX, 1)
-  p0 = iProjector(C, targets, v = tab$f)
+  ## compute hypothesis distribution
+  p0 <- iProjector(C, targets, v = tab$f)
+  ## compute I-divergence
   idiv <- totem:::computeStatistic(C, targets, tab)
-
+  ## compute p-value
   p.value <- totem:::computePvalue(idiv, sum(tab$Freq), 1, alternative = "two.sided")
-
-  list(iDivergence = idiv, p.value, A = sum(p0$p * tab$BMR / tab$Mass^b), p0 = p0$p)
+  ## compute expected A
+  A0 <- sum(p0$p * tab$BMR / tab$Mass^b)
+  ## return result
+  list(iDivergence = idiv, p.value, A0 = A0 , p0 = p0$p)
 }
+## test b = 2/3
+allometricTwoThird = testAllometric(2/3)
 
+## test b = 3/4
+allometricThreeFourth = testAllometric(3/4)
+
+## test the thermodynamic model
 testThermodynamic <- function(kPrime = 0.1, f = 0.21){
+  ## <x>
   meanX <- sum(tab$f * tab$Mass)
   ## constraints
   C <- rbind(
     ## hypothesis constraint
     tab$BMR - (1 - f) * kPrime / 0.0201 * tab$Mass^(2/3) - (tab$BMR  / tab$Mass - (1 - f) * kPrime / 0.0201 * tab$Mass^(2/3 - 1)) * meanX,
+    ## structural constraint
+    ### <x>_f = <x>_p
     tab$Mass,
+    ### normalization
     rep(1, nrow(tab))
   )
   targets <- c(0, meanX, 1)
@@ -51,9 +69,7 @@ testThermodynamic <- function(kPrime = 0.1, f = 0.21){
   list(iDivergence = idiv, p.value, K = sum(p0$p * (tab$BMR - (1 - f) * kPrime / 0.0201 * tab$Mass^(2/3)) / (f * tab$Mass)), p0 = p0$p)
 }
 
-## pure allometric models
-allometricTwoThird = testAllometric(2/3)
-allometricThreeFourth = testAllometric(3/4)
+
 
 ## calculate 95% CI
 optF <- function(b){
